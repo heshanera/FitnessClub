66d586cf9bdbd7cf5753949fc8bb9ce0e6adb482
Sulochana Kodituwakku <sulochana.456@live.com>
2016-12-01 14:32:08 +0530

    config changes

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'localhost/Fitness-Club';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
